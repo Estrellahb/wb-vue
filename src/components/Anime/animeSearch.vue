96b42a3f52d24c6e3b0a484873c35cf83cf9f37c
陆秋 <wujacket218@gmail.com>
2024-05-22 20:37:23 +0800

    模糊搜索

---
+++ src/components/Anime/animeSearch.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="search">
+      <el-input v-model="searchQuery" placeholder="请输入动画名称"></el-input>
+      <el-button @click="searchAnime" type="primary">搜索</el-button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        searchQuery: '' // 绑定输入框中的搜索查询
+      };
+    },
+    methods: {
+      searchAnime() {
+        // 调用父组件传递的搜索方法，并传递搜索查询
+        this.$emit('search', this.searchQuery);
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .search {
+  display: flex;
+  margin-bottom: 20px;
+}
+
+.search .el-input {
+  margin-right: 10px;
+  margin-left: 10%;
+  width: 80%;
+}
+
+</style>
